4743f66206cd02af2dd74a5d21c87026dfa77712
Lucas-PC <lucasmvnascimento@gmail.com>
2021-03-11 13:28:19 -0300

    Exercise Two

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import Provider from './context/Provider';
 import TrafficSignal from './TrafficSignal';
 
 function App() {
   return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
+    <Provider>
+      <div className="container">
+        <Cars />
+        <TrafficSignal />
+      </div>
+    </Provider>
   );
 }
 
